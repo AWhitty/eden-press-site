cb71c2403b17565407c5331f38cf5138e6930f04
Austin Whittier <austin.whitt@gmail.com>
2014-08-13 23:43:20 -0600

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+var navResponsive = $('nav.responsive');
+
+navResponsive.hide();
+
+$('.nav-show').click(function() {
+	navResponsive.show();
+});
+
+navResponsive.children('a.close').click(function() {
+	navResponsive.hide();
+});
